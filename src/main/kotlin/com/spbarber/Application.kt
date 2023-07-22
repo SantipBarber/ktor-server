ffb01e085308305b9aaba28c27379eae81072e1a
SpBarber <santipbr@gmail.com>
2023-07-22 17:36:19 +0200

    Modificaciones para KotliJS

--- src/main/kotlin/com/spbarber/Application.kt
+++ src/main/kotlin/com/spbarber/Application.kt
@@ -4,6 +4,8 @@ import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import com.spbarber.plugins.*
+import io.ktor.http.*
+import io.ktor.server.plugins.cors.routing.*
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
@@ -13,4 +15,11 @@ fun main() {
 fun Application.module() {
     configureSerialization()
     configureRouting()
+    install(CORS){
+        anyHost()
+        allowNonSimpleContentTypes
+        allowHeader(HttpHeaders.AccessControlAllowOrigin)
+        allowMethod(HttpMethod.Put)
+        allowMethod(HttpMethod.Delete)
+    }
 }
