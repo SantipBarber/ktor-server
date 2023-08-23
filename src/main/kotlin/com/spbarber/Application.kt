87c22b816bc628ab70bc389bc27bfdf568eae64d
SpBarber <santipbr@gmail.com>
2023-08-23 14:08:45 +0200

    Preparando despliegue puerto predeterminado para funcionamieto en HEROKU

--- src/main/kotlin/com/spbarber/Application.kt
+++ src/main/kotlin/com/spbarber/Application.kt
@@ -4,17 +4,18 @@ import com.spbarber.plugins.configureRouting
 import com.spbarber.plugins.configureSerialization
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.plugins.cors.routing.*
 
-//fun main() {
-//    embeddedServer(Netty, port = (System.getenv("PORT")?:"5000").toInt(), module = Application::module)
-//        .start(wait = true)
-//}
-
-
 val port = System.getenv("PORT")?.toInt() ?: 23567
-fun main(args: Array<String>): Unit = EngineMain.main(args)
+fun main() {
+    embeddedServer(Netty, port = port, module = Application::module)
+        .start(wait = true)
+}
+
+
+// fun main(args: Array<String>): Unit = EngineMain.main(args)
 
 fun Application.module() {
     configureSerialization()
